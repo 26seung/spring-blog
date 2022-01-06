35e1841cbf6a82d2cbe02a1a223c1a3d2e60489b
Euseung <yousi7@naver.com>
2022-01-07 02:06:00 +0900

    page

--- src/main/java/com/one/blog/controller/BoardController.java
+++ src/main/java/com/one/blog/controller/BoardController.java
@@ -2,6 +2,9 @@ package com.one.blog.controller;
 
 import com.one.blog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +16,8 @@ public class BoardController {
     BoardService boardService;
 
     @GetMapping({"/blog","/"})
-    public String index(Model model){
-        model.addAttribute("boards",boardService.글목록());
+    public String index(Model model, @PageableDefault(size = 3,sort = "id",direction = Sort.Direction.DESC) Pageable pageable){
+        model.addAttribute("boards",boardService.글목록(pageable));
         return "index";
     }
 
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -5,7 +5,7 @@
 <div class="container">
     <h3>블로그 만들기 연습장</h3>
 
-    <c:forEach var="board" items="${boards}">
+    <c:forEach var="board" items="${boards.content}">
     <div class="card">
       <div class="card-body">
         <h4 class="card-title">${board.title}</h4>
@@ -13,6 +13,11 @@
       </div>
     </div>
   </c:forEach>
+
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
 
 </div>
 
